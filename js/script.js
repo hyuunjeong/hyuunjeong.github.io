ef5c98365fb86e291fc1e4fb23cdf97cab0474db
sally park <hpark17@risd.edu>
2021-01-23 20:21:24 -0500

    style nav

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var a = 6;
 console.log(a);
 
+/*<---------RISD logo in intro-------->*/
+
 
 $("document").ready(function(){
   $(".RISD").hover(function(){
@@ -11,23 +13,28 @@ $("document").ready(function(){
 });
 
 
+
+/*<---------Don't feed the birds-------->*/
+
+        /*<---------border on hover-------->*/
+
 $("document").ready(function(){
   $(".stopicon").hover(function(){
-    $(".dontfeedcont").css("border","4px dotted");
+    $("#dontfeedcont").css("border","4px dotted");
   }, function(){
-    $(".dontfeedcont").css("border","hidden");
+    $("#dontfeedcont").css("border","hidden");
   });
 });
-
 
 $("document").ready(function(){
   $(".dontfeed").hover(function(){
-    $(".dontfeedcont").css("border","4px dotted");
+    $("#dontfeedcont").css("border","4px dotted");
   }, function(){
-    $(".dontfeedcont").css("border","hidden");
+    $("#dontfeedcont").css("border","hidden");
   });
 });
 
+        /*<---------caption on hover-------->*/
 
 $("document").ready(function(){
   $(".dontfeed").hover(function(){
@@ -43,5 +50,46 @@ $("document").ready(function(){
     $("#dontfeedcap").css("color","black");
   }, function(){
     $("#dontfeedcap").css("color","rgb(222, 222, 222)");
+  });
+});
+
+
+/*<---------MONA-------->*/
+
+
+
+        /*<---------border on hover-------->*/
+$("document").ready(function(){
+  $(".letter").hover(function(){
+    $("#monacont").css("border","4px dotted");
+  }, function(){
+    $("#monacont").css("border","hidden");
+  });
+});
+
+$("document").ready(function(){
+  $(".monalogo").hover(function(){
+    $("#monacont").css("border","4px dotted");
+  }, function(){
+    $("#monacont").css("border","hidden");
+  });
+});
+
+        /*<---------caption hover-------->*/
+
+$("document").ready(function(){
+  $(".letter").hover(function(){
+    $("#monacap").css("color","black");
+  }, function(){
+    $("#monacap").css("color","rgb(222, 222, 222)");
+  });
+});
+
+
+$("document").ready(function(){
+  $(".monalogo").hover(function(){
+    $("#monacap").css("color","black");
+  }, function(){
+    $("#monacap").css("color","rgb(222, 222, 222)");
   });
 });
