eb6d4cfe22214b059ae43eff952ebbc94a9c9247
sally park <hpark17@risd.edu>
2021-05-28 01:18:28 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 var a = 6;
 console.log(a);
-alert("1. rearrange the memes 2. press the spacebar to annotate 3. take a screenshot and send (☞ﾟ∀ﾟ)☞ ");
+alert("1. rearrange the memes 2. annotate on the sticky note 3. take a screenshot and send (☞ﾟ∀ﾟ)☞ ");
